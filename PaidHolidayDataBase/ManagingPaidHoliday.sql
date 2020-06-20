b9ea8ba984099660e1727f9f4b7bc6b9da71f0df
鈴木悠也 <monorix1013@live.jp>
2020-06-20 17:38:17 +0900

    add table layout

---
+++ PaidHolidayDataBase/ManagingPaidHoliday.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ManagingPaidHoliday]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, -- id 
+    [EmployeeId] INT NOT NULL , -- 社員番号 
+    [GrantDate] DATE NOT NULL, -- 有給付与日
+    [RemainingDays] INT NOT NULL -- 残日数
+)
---
+++ PaidHolidayDataBase/UsedPaidHolidaysHistory.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UsedPaidHolidaysHistory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [EmployeeId] INT NOT NULL, -- 社員ID
+    [UseDate] DATE NOT NULL, -- 使用日
+    [PaidHolidayId] INT NOT NULL -- 使用された有給Idあってもなくても良い気はする,
+)
